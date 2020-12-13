6da7bbc35bb316b8544db7cd6eb8f3cf8a44222e
13udha <13udha666@gmail.com>
2020-12-13 10:40:32 +0100

    Update manage.py
    fix for heroku

--- gitgud/skill-gitgud-python/manage.py
+++ gitgud/skill-gitgud-python/manage.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 from skill_sdk.manage import manage
 
+conf_file = 'skill.conf'
+
+if 'PORT' in os.environ:
+    config = configparser.ConfigParser()
+    config.read(conf_file)
+    config['http']['port'] = os.environ['PORT']
+    with open(conf_file, 'w') as configfile:
+        config.write(configfile)
+
+
 manage()
